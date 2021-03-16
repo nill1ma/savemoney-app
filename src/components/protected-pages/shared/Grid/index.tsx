2e382d74d61cbd202dbb1b48ffc1b19ec89116de
Nil <avanilson.limasantos@gmail.com>
2021-03-15 22:12:47 -0300

    Refatoração de código do componente de Grid

--- src/components/protected-pages/shared/Grid/index.tsx
+++ src/components/protected-pages/shared/Grid/index.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Expenses } from "../../../../models/Expenses";
 import { MoneyDeposit } from "../../../../models/MoneyDeposit";
 import { GridContainer, GridHeaderItem, GridItemBox, GridItemsContainer, GridItemText } from "./styles";
-
-
 
 type TDataProps = {
     dataProps: Expenses[] | MoneyDeposit[]
@@ -12,11 +10,7 @@ type TDataProps = {
 export default function Grid(props: TDataProps) {
     const { dataProps } = props
 
-    const [data, setData] = useState(dataProps)
-
-    useEffect(() => {
-        setData(dataProps)
-    }, [dataProps])
+    const [data] = useState(dataProps)
 
     const dataFields = Object.keys(data[0])
 
@@ -26,18 +20,15 @@ export default function Grid(props: TDataProps) {
                 <GridItemsContainer>
                     <>
                         <GridHeaderItem>
-                            {dataFields.map(h => {
-                                return (
-                                    <GridItemText lengthOfObjects={dataFields.length}>{h.split(/(?=[A-Z])/).map(hd=>`${hd} `)}</GridItemText>
-                                )
-                            })}
+                            {dataFields.map(h =>
+                                <GridItemText lengthOfObjects={dataFields.length}>{h.split(/(?=[A-Z])/).map(hd => `${hd} `)}</GridItemText>
+                            )}
                         </GridHeaderItem>
                         {data.map((d: any) => {
                             return (
                                 <GridItemBox key={`${dataFields}${Math.random()}`}>
-                                    {dataFields.map((h: string) => (
+                                    {dataFields.map((h: string) =>
                                         <GridItemText key={`${h}${Math.random()}`} lengthOfObjects={dataFields.length}>{d[h]}</GridItemText>
-                                    )
                                     )}
                                 </GridItemBox>
                             )
